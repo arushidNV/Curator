8c7f4f15f4aed70d925e8439bae0c2adefc92d6d
Sushmitha Deva <sdeva@nvidia.com>
2026-08-09 21:08:42 -0700

    Update scripts

--- examples/audio/metadata_extraction/run_reader_writer.py
+++ examples/audio/metadata_extraction/run_reader_writer.py
@@ -1,3 +1,5 @@
+##TODO: delete this file after usage. This is a temporary file for the reader writer pipeline.
+
 # Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
